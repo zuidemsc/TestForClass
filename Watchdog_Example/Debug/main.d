8d4889f3e9701e80c1f58ff1126517a61eac9d47
Scott Zuidema <zuidemsc@gvsu.edu>
2018-11-28 14:30:51 -0500

    Initial Commit

---
+++ Watchdog_Example/Debug/main.d
@@ -0,0 +1,35 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/msp.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/msp432p401r.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main.obj: /home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: /home/scott/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+
+../main.c: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/msp.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+/home/scott/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/home/scott/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
